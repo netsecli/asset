e614ee5b710293640bae4f50a921f657e9f31319
netsecli <49426508+netsecli@users.noreply.github.com>
2019-04-09 13:21:08 +0800

    Create ingest.sh

---
+++ ingest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILES=/opt/nmap-xml/*.xml
+DATE=`date +%Y.%m.%d`
+curl -XDELETE "localhost:9200/nmap*"
+for f in $FILES
+do
+    echo "Processing $f file..."
+    python /opt/nmap-elk-sh/VulntoES.py -i $f -e localhost -r nmap -I nmap-$DATE
+done
