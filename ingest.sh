784c24020029e7fe34b5563de657603cc67ffaf0
netsecli <49426508+netsecli@users.noreply.github.com>
2019-11-14 18:31:26 +0800

    Update ingest.sh

--- ingest.sh
+++ ingest.sh
@@ -6,5 +6,5 @@ curl -XDELETE "localhost:9200/nmap*"
 for f in $FILES
 do
     echo "Processing $f file..."
-    python /opt/nmap-elk-sh/VulntoES.py -i $f -e localhost -r nmap -I nmap-$DATE
+    python /opt/nmap-elk-sh/vulntoES.py -i $f -e localhost -r nmap -I nmap-$DATE
 done
